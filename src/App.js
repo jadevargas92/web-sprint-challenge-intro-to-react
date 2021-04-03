854e3c56f828f82f8068566451a96579675d483d
Jade <jadevargas92@gmail.com>
2021-04-03 14:45:31 -0500

    Testing Code Grade

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  // Wanted to change the file and make sure codegrade was working
 
   return (
     <div className="App">
